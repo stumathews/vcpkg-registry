ef4bc77525de6938e5d6b83ec882e3653ac9bbf5
Stuart Mathews <stumathews@gmail.com>
2025-07-05 19:46:44 +0200

    update version for libmonad

--- ports/libmonad/portfile.cmake
+++ ports/libmonad/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO stumathews/libmonad
-    REF test
+    REF test1
     SHA512 c2ba73417a29f7abb8df401b98cc2e52e0686b44ba1fdbc766fe3f1b57ef14050d094ad4cac51649ea02d95c8bf32aef25033a5a28beeac12729ed496708a67e
     HEAD_REF master
 )
