3b7e25d6e9d17014641e2fa4d5d29d3d66d40bd4
Stuart Mathews <stumathews@gmail.com>
2025-07-11 12:51:14 +0200

    update

--- ports/cppgamelib/portfile.cmake
+++ ports/cppgamelib/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO stumathews/cppgamelib
-    REF v1.2.2
-    SHA512 0b73b0130d74800a3e40b564e31efaa9a21cefcb6ac90f9897d9b96bd7f08af85c862a494923d5959f915267d615b127fe945a5cc2174a4eee880799921a4952
+    REF v1.2.3
+    SHA512 0
     HEAD_REF master
 )
 
