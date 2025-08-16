2309f669f7a8acf0f27eed9b8cde4c03a4124786
Stuart Mathews <stumathews@gmail.com>
2025-08-16 16:25:05 +0100

    add test project

---
+++ ports/test/portfile.cmake
@@ -0,0 +1,16 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO stumathews/TemplateLibrary
+    REF v0.1
+    SHA512 0
+    HEAD_REF master
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_cmake_install()
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
