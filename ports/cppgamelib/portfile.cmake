f1b93b08500ee2f1382fff8ac6fe07b41fed622b
Stuart Mathews <stumathews@gmail.com>
2025-08-16 22:45:12 +0100

    update port file to delete debug/include

--- ports/cppgamelib/portfile.cmake
+++ ports/cppgamelib/portfile.cmake
@@ -12,5 +12,9 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
+vcpkg_cmake_config_fixup(PACKAGE_NAME "cppgamelib" CONFIG_PATH lib/cmake/cppgamelib)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
