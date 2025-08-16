438cd1292894f5acb01fe83b04e1c1c9730b6a59
Stuart Mathews <stumathews@gmail.com>
2025-08-16 16:27:23 +0100

    update sha

--- ports/test/portfile.cmake
+++ ports/test/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO stumathews/TemplateLibrary
     REF v0.1
-    SHA512 0
+    SHA512 1006ee88ff2b6a1c2f84b8464d4ca547f46b53d297b667cef5e04bb6fe235282a6f867ca70dee6b323f313ed578eb4b86f4cb8646f8375731768398f51f8ce11
     HEAD_REF master
 )
 
