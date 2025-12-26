0e4c1c608ba5b115d0855edf283bb0981bbc9487
Stuart Mathews <stumathews@gmail.com>
2025-12-26 17:46:09 +0000

    update cppgamelib to 1.2.33

--- ports/cppgamelib/portfile.cmake
+++ ports/cppgamelib/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO stumathews/cppgamelib
-    REF v1.2.32
+    REF v1.2.33
     SHA512 786a83e65ba0d3c6226f83e7a0ba3d11eebcc27dd7fc7572a8e3f261852727ed0ca2a1c4c50d66b83166b022eba2a5193a9ba9fd5cd990dfb61c160fcebb91d0 
     HEAD_REF linux
 )
